dc327ecfbf812257db8449063ec40aea555819cc
HyperBitGore <bruiserjoe01@gmail.com>
2024-10-11 09:27:48 -0500

    Meeting 10/11/24

---
+++ webserver/src/main/java/com/cryptlab/webserver/WebserverApplication.java
@@ -0,0 +1,15 @@
+package com.cryptlab.webserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserverApplication.class, args);
+	}
+
+	
+
+}
