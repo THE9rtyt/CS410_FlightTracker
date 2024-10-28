cd4b650df54ed3a118b32a5e508d07c55c3d2291
Saint-18 <67234406+Saint-18@users.noreply.github.com>
2024-10-28 10:49:13 -0500

    Add files via upload

---
+++ Flight.js
@@ -0,0 +1,89 @@
+function NorthAmerica(){
+
+}
+function SouthAmerica(){
+
+}
+function Africa(){
+
+}
+function Asia(){
+
+}
+function Europe(){
+
+}
+
+function openLink() {
+    const input = document.getElementById('codeInput').value.trim().toUpperCase();
+    
+    if (input === "ATL") {
+      window.open("https://www.atl.com/maps-fullscreen/", "_blank");
+    } else {
+      alert("Error. Please enter a vible code to open the map.");
+    }
+  }
+
+/*
+Africa
+1 	 South Africa 	O. R. Tambo International Airport 	Johannesburg
+2 	 Egypt 	Cairo International Airport 	Cairo
+3 	 Ethiopia 	Bole International Airport 	Addis Ababa
+4 	 South Africa 	Cape Town International Airport 	Cape Town
+5 	 Morocco 	Mohammed V International Airport 	Casablanca
+6 	 Algeria 	Houari Boumediene Airport 	Algiers		
+7 	 Kenya 	Jomo Kenyatta International Airport 	Nairobi  		
+8 	 Egypt 	Hurghada International Airport 	Hurghada 	
+9 	 Nigeria 	Murtala Muhammed International Airport 	Lagos		
+10 	 Tunisia 	Tunis Carthage Airport 	Tunis	
+
+Asia
+1 	China Beijing Capital International Airport 	Beijing 	China 	PEK 	ZBAA
+2 	United Arab Emirates Dubai International Airport 	Dubai 	United Arab Emirates 	DXB 	OMDB
+3 	Japan Tokyo International (Haneda) Airport 	Tokyo 	Japan 	HND 	RJTT
+4 	Hong Kong Hong Kong International Airport 	Hong Kong 	China 	HKG 	VHHH
+5 	China Shanghai Pudong International Airport 	Shanghai 	China 	PVG 	ZSPD
+6 	India Indira Gandhi International Airport 	Delhi 	India 	DEL 	VIDP
+7 	China Guangzhou Baiyun International Airport 	Guangzhou 	China 	CAN 	ZGGG
+8 	South Korea Incheon International Airport 	Seoul/Incheon 	Republic of Korea 	ICN 	RKSI
+9 	Indonesia Soekarno–Hatta International Airport 	Jakarta/Tangerang 	Indonesia 	CGK 	WIII
+10 	Singapore Singapore Changi Airport 	Singapore 	Singapore 	SIN 	WSSS 
+
+Europe
+1 	 	 Turkey 	Istanbul Airport 	Istanbul
+2 	 	 France 	Charles de Gaulle Airport 	Paris
+3 	 	 United Kingdom 	Heathrow Airport 	London 
+4 	 	 Netherlands 	Amsterdam Airport Schiphol 	Amsterdam
+5 	 	 Russia 	Sheremetyevo International Airport 	Moscow
+6 	 	 Germany 	Frankfurt am Main Airport 	Frankfurt
+7 	 	 Spain 	Adolfo Suárez Madrid–Barajas Airport 	Madrid
+8 	 	 Russia 	Domodedovo International Airport 	Moscow 
+9 	 	 Spain 	Barcelona–El Prat Josep Tarradellas Airport 	Barcelona
+10 	 	 Russia 	Vnukovo International Airport 	Moscow 	
+
+North America
+1 		Hartsfield–Jackson Atlanta International Airport 	Atlanta 	 United States ATL
+https://www.atl.com/maps-fullscreen/
+2 	 	Dallas/Fort Worth International Airport 	Dallas–Fort Worth 	 United States
+3 	 	Denver International Airport 	Denver 	 United States 
+4 	 	O'Hare International Airport 	Chicago 	 United States
+5 	 	Los Angeles International Airport 	Los Angeles 	 United States
+6 	 	Charlotte Douglas International Airport 	Charlotte 	 United States
+7 	 	McCarran International Airport 	Las Vegas 	 United States
+8 	 	Mexico City International Airport 	Mexico City 	 Mexico[17]
+9 	 	Phoenix Sky Harbor International Airport 	Phoenix 	 United States
+10 	 	Orlando International Airport 	Orlando 	 United States
+
+South America
+1 	Brazil 	São Paulo–Guarulhos International Airport 	São Paulo
+2 	Colombia 	El Dorado International Airport 	Bogotá
+3 	Chile 	Arturo Merino Benítez International Airport 	Santiago
+4 	Brazil 	Brasília International Airport 	Brasília
+5 	Peru 	Jorge Chávez International Airport 	Lima
+6 	Brazil 	São Paulo–Congonhas Airport 	São Paulo
+7 	Brazil 	Viracopos International Airport 	Campinas
+8 	Brazil 	Santos Dumont Airport 	Rio de Janeiro
+9 	Brazil 	Recife/Guararapes–Gilberto Freyre International Airport 	Recife
+10 	Brazil 	Belo Horizonte International Airport 	Belo Horizonte
+
+*/
