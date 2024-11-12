a119fb8818daa7a314eb219d2e3ba6f7b377442b
Joshua Miertschin <joshuamiertschin@gmail.com>
2024-11-12 16:19:56 -0600

    add Amadeus object managment for handing out

--- webserver/src/main/java/com/cryptlab/webserver/WebserverApplication.java
+++ webserver/src/main/java/com/cryptlab/webserver/WebserverApplication.java
@@ -4,38 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.amadeus.Amadeus;
-import com.amadeus.Params;
-import com.amadeus.exceptions.ResponseException;
-import com.amadeus.referencedata.Locations;
-import com.amadeus.resources.Location;
 
 @SpringBootApplication
 public class WebserverApplication {
 
+	private static Amadeus amadeus;
+
 	public static void main(String[] args) {
-		System.out.println("hello amadeus");
+    amadeus = Amadeus
+        .builder(System.getenv())
+        .build();
 
 		SpringApplication.run(WebserverApplication.class, args);
-		
-	    Amadeus amadeus = Amadeus
-	            .builder("UHYoiuW5IEBc4SmWhrqDYGw7oYAl2GNJ", "4SU1BnL9EU3Tn2sO")
-	            .build();
-
-	    Location[] locations;
-			try {
-				locations = amadeus.referenceData.locations.get(Params
-				  .with("keyword", "LON")
-				  .and("subType", Locations.ANY));
-
-				for(var location : locations) {
-					System.out.println(location);
-				}
-			} catch (ResponseException e) {
-				e.printStackTrace();
-			}
-
 	}
 
-	
-
+	public static Amadeus getAmadeusObject() throws NullPointerException{
+		if(amadeus != null) {
+			return amadeus;
+		}
+		throw new NullPointerException();
+	}
 }
